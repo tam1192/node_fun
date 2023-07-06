11fa799a920b029e8a1d9639ddce34f10c861f5f
tam <tamkame123@outlook.com>
2023-07-06 14:11:45 +0900

    古い奴

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
 // fetchAPIを使う
 // voicevox = http://localhost:50021/
 
-const fs = require("node:fs");
 const vox = require("./module/vox");
 const config = require("./config.json");
 
-const voicevox = new vox(config.ServerAddress,config.ServerPort,config.ServerHTTPS);
+const voicevox = new vox(config.ServerAddress,config.ServerPort,config.ServerHTTPS,config.exportdir);
 
-voicevox.on("build_query",(query,text)=>{
-    console.group("query完成");
-    console.info(text);
-    console.debug(query);
-    console.groupEnd();
-});
+// voicevox.on("build_query",(text)=>
+// {
+//     console.group("query完成");
+//     console.info(text);
+//     console.groupEnd();
+// });
 
-voicevox.on("build_wav",(data,text)=>{
-    fs.writeFileSync("./"+text+".wav",data);
+voicevox.on("build_wav",(text)=>
+{
     console.group("音声完成");
     console.info(text)
     console.groupEnd();
 });
 
-voicevox.on("err",(x,y)=>{
+voicevox.on("err",(x,y)=>
+{
     console.error("発生個所:",x);
     console.debug("ステータスコード:",y);
 })
 
-async function say(str=""){
-    // const que = await (await voicevox.audio_query(str,1)).synthesis(1,true);
-    await voicevox.synthesis(str,1);
-}
-
-console.clear();
-say("こんにちは、僕ずんだもん！");
-// say("東北ばんざい！");
-// say("ずんこのふとももはもっちもち");
-// say("私はスパムが大っ嫌いなのよ");
-// say("あかねちゃんかわいいやったー");
+voicevox.synthesis("草風呂入ってから向かうわ", 1, true) ;
--- module/vox.js
+++ module/vox.js
@@ -9,13 +9,15 @@ module.exports = class extends EventEmitter{
      * @param {string} address ipアドレス・ドメインetc...
      * @param {number} port ポート番号
      * @param {boolean} https trueでhttps
+     * @param {string} exportdir 出力先
      * @returns 
      */
-    constructor(address='',port=50021,https=false)
+    constructor(address='',port=50021,https=false,exportdir='./exports/')
     {
         super();
         const proto = https ? 'https://' : 'http://';
         this.url = proto + address + ":" + port;
+        this.exportdir = exportdir;
         return this;
     }
 
@@ -40,6 +42,7 @@ module.exports = class extends EventEmitter{
             'text':text,
             'speaker':speaker
         });
+
         const reqinit = {
             method: 'POST',
             cache: 'no-cache',
@@ -47,26 +50,48 @@ module.exports = class extends EventEmitter{
                 'Accept':'application/json'
             }
         }
+
         const req = await fetch(url,reqinit).catch(()=>{
             return undefined;
         });
-        if(typeof(req) != "object"){
+
+        if(typeof(req) != "object")
+        {
             this.emit("err","audio_query","server error");
             return undefined
-        }else if(req.ok){
+        }
+        else if(req.ok)
+        {
             const data = await req.json();
             this.emit("build_query", data, text);
             return new query(this, data, text);
-        }else{
+        }
+        else
+        {
             this.emit("err","audio_query",req.statusText);
             return undefined;
         }
     }
     
-    synthesis = async function(text='',speaker=0){
+    /**
+     * 簡易版
+     * @param {string} text 
+     * @param {number} speaker 
+     * @returns wav
+     */
+    synthesis = async function(text='',speaker=0,kana=true)
+    {
         const q = await this.audio_query(text,speaker);
-        if(typeof(q) == "object"){
-            return await q.synthesis(speaker);
+        if(typeof(q) == "object")
+        {
+            if(kana)
+            {
+                return await q.kana(speaker,true,true);
+            }
+            else
+            {
+                return await q.synthesis(speaker,true);
+            }
         }
     }
 }
