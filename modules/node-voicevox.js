69b6426642bc774d331ab4809efe2195850ecb72
tam <tamkame123@outlook.com>
2023-07-07 08:59:34 +0900

    rakutenma導入

--- modules/node-voicevox.js
+++ modules/node-voicevox.js
@@ -1,11 +1,1 @@
 "use strict";
-
-/**
- * @class クエリ情報をまとめるクラス
- */
-exports.query = require('./node-voicevox/query');
-
-/**
- * @class voicevoxエンジン情報をまとめるクラス
- */
-exports.voicevox_engine = require('./node-voicevox/voicevox_engine');
---
+++ modules/rakutenma.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * @class rakutenma
+ */
+exports.Rakuten = require('./node-voicevox/query');
+
+/**
+ * @class voicevoxエンジン情報をまとめるクラス
+ */
+exports.voicevox_engine = require('./node-voicevox/voicevox_engine');
